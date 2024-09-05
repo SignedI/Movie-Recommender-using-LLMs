f571007d44d34e1246e617863066fdad3f3513a5
SignedI <106474711+SignedI@users.noreply.github.com>
2024-09-06 03:15:19 +0530

    Update code.py

--- code.py
+++ code.py
@@ -1,6 +1,11 @@
 import pandas as pd
 import requests
 from google.colab import files
+from transformers import LlamaForCausalLM, LlamaTokenizer
+import pinecone
+from openai.embeddings_utils import get_embedding
+
+API_KEY = '' 
 
 def get_imdb_data(movie_title):
     url = f"http://www.omdbapi.com/?t={movie_title}&apikey={API_KEY}"
@@ -12,29 +17,23 @@ def get_imdb_data(movie_title):
             'imdbVotes': data.get('imdbVotes', 'N/A')
         }
     else:
-        return {
-            'imdbRating': 'N/A',
-            'imdbVotes': 'N/A'
-        }
-        
-        
+        return {'imdbRating': 'N/A', 'imdbVotes': 'N/A'}
+
 def rate_movie(movie_title, user_rating):
     if movie_title in data['title'].values:
         prev_rating = data.loc[data['title'] == movie_title, 'new_user_rating'].values[0]
         prev_rating_count = data.loc[data['title'] == movie_title, 'rating_count'].values[0]
-        
+
         new_rating_count = prev_rating_count + 1
         new_user_rating = ((prev_rating * prev_rating_count) + user_rating) / new_rating_count
-        
+
         data.loc[data['title'] == movie_title, 'new_user_rating'] = new_user_rating
         data.loc[data['title'] == movie_title, 'rating_count'] = new_rating_count
 
         return new_rating_count
-        
     else:
         print(f"Movie '{movie_title}' not found in the dataset.")
         return None
-        
 
 def update_embedding_after_30_ratings(movie_title):
     rating_count = data.loc[data['title'] == movie_title, 'rating_count'].values[0]
@@ -50,6 +49,7 @@ def update_embedding_after_30_ratings(movie_title):
         new_imdb_rating = (a1 + 30 * a2) / (imdb_votes + 30)
         data.loc[data['title'] == movie_title, 'imdbRating'] = new_imdb_rating
         data.loc[data['title'] == movie_title, 'imdbVotes'] += 30
+        data.loc[data['title'] == movie_title, 'rating_count'] = 0
         
         new_embedding = get_embedding(data.loc[data['title'] == movie_title, 'combined'].values[0], engine="text-embedding-ada-002")
         movie_id = data.loc[data['title'] == movie_title, 'title'].values[0] 
@@ -59,19 +59,42 @@ def update_embedding_after_30_ratings(movie_title):
     else:
         print(f"Embedding not updated yet. {rating_count} ratings received for '{movie_title}'.")
 
+pinecone.init(api_key="", environment="")
+index_name = "movie-recommendations"
+pinecone.create_index(index_name, dimension=len(data['embedding'][0]))
+
+index = pinecone.Index(index_name)
+
+model_name = "meta-llama/Llama-2-7b-chat-hf"
+model = LlamaForCausalLM.from_pretrained(model_name)
+tokenizer = LlamaTokenizer.from_pretrained(model_name)
+
+def recommend_movie(user_query):
+    query_embedding = get_embedding(user_query, engine="text-embedding-ada-002")
+    search_results = index.query(query_embedding, top_k=5, include_values=True)
+    
+    context = ""
+    for match in search_results['matches']:
+        movie_info = data.loc[data['title'] == match['id'], 'combined'].values[0]
+        context += f"{movie_info}\n"
+
+    prompt = f"Given the user's preference: '{user_query}', and considering the following movies:\n{context}\nWhat movie would you recommend?"
+    inputs = tokenizer(prompt, return_tensors="pt")
+    output = model.generate(**inputs, max_new_tokens=100)
+    recommendation = tokenizer.decode(output[0], skip_special_tokens=True)
+
+    return recommendation
 
 def user_input_query():
-    movie_title = input("Enter the movie title: ")
+    user_query = input(f"What type of movies are you interested in? ")
+    recommendation = recommend_and_rate_movie(user_query)
+    print("Recommendation:", recommendation)
+    
+    movie_title = input("Which movie did you pick: ")
     user_rating = float(input(f"Provide a rating for {movie_title}: "))
-    
+    rate_movie(movie_title, user_rating)
+    update_embedding_after_30_ratings(movie_title)
 
-    rating_count = rate_movie(movie_title, user_rating)
-    
-    if rating_count is not None:
-        update_embedding_after_30_ratings(movie_title)
-
-
-API_KEY = ''
 netflix_df = pd.read_csv('netflix_titles.csv')
 
 netflix_df['imdbRating'] = ''
@@ -88,34 +111,20 @@ for index, row in netflix_df.iterrows():
 output_file = 'netflix_with_ratings.csv'
 netflix_df.to_csv(output_file, index=False)
 data = pd.read_csv("netflix_with_ratings.csv")
+
 data['combined'] = data['type'] + " - " + data['title'] + " - " + \
                    data['director'] + " - " + data['cast'] + " - " + \
                    data['country'] + " - " + data['release_year'].astype(str) + \
-                   " - " + data['listed_in'] + " - Rating: " + data['imdbRating'].astype(str) + \
-                   " - Votes: " + data['imdbVotes'].astype(str)
+                   " - " + data['listed_in'] + " - Rating: " + data['imdbRating'].astype(str)
 
-
-from openai.embeddings_utils import get_embedding
 data['embedding'] = data['combined'].apply(lambda x: get_embedding(x, engine="text-embedding-ada-002"))
 
 movie_ids = data['title'].tolist()  
 embeddings = data['embedding'].tolist()
 vectors = list(zip(movie_ids, embeddings))
 
-import pinecone
-pinecone.init(api_key="ENTER API KEY", environment="")
-
-index_name = "movie-recommendations"
-pinecone.create_index(index_name, dimension=len(embeddings[0]))
-
-index = pinecone.Index(index_name)
 index.upsert(vectors)
 
-from transformers import LlamaForCausalLM, LlamaTokenizer
-
-model_name = "meta-llama/Llama-2-7b-chat-hf"
-model = LlamaForCausalLM.from_pretrained(model_name)
-tokenizer = LlamaTokenizer.from_pretrained(model_name)
-
 user_input_query()
+
 
